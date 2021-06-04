94ee5e44e733280c87805986b524d2c0f529daa4
bardenha <bardenha@gmail.com>
2021-06-04 15:35:20 +0100

    Project Skeleton

---
+++ README.md
@@ -0,0 +1,12 @@
+# Online Library Application (WIP)
+
+04.06.21
+==
+Web design project for The Odin Project: JavaScript - Project: Library
+==
+Having just completed a very intensive final year at University, I now have time again to continue supplementary self-study and will continue to complete TheOdinProject.
+--
+Task: Develop a fully functioning online library application using javascript, HTML and CSS.
+Project insights:
+--
+- Currently In Progress
---
+++ library.js
@@ -0,0 +1,12 @@
+/*
+Plan:
+1- Create basic library array
+2- Create a function 'Book' which takes user input to create new book object
+3- Create a function 'addBookToLibrary' which takes book object and stores in array
+4- Create function to loop through array and display book on page
+5- "New book" button which calls previous functions
+6- "Remove book" calling function to remove book from array 
+7- Button functionality to toggle book read status
+8- Integrate Firebase to save user data
+*/
+
