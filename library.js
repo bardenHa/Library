33fd6e0b4074fc2f94a1b41bc46f38dcfe81842d
bardenha <bardenha@gmail.com>
2021-06-10 19:08:54 +0100

    Modal open/close functionality

--- library.js
+++ library.js
@@ -11,6 +11,31 @@ Plan:
 9- Add example books
 */
 
+//open book form
+let formOpen = false;
+
+document.getElementById('add-book-button').addEventListener('click', () => {
+    document.querySelector('.bg-modal').style.display = 'flex';
+    setTimeout(() => {formOpen = true}, 100);
+});
+
+//close book form using cancel button
+document.getElementById('cancel-button').addEventListener('click', () => {
+    document.querySelector('.bg-modal').style.display = 'none';
+    formOpen = false;
+});
+
+//close book form by clicking outsite window
+var specifiedElement = document.getElementById('modal-container');
+document.addEventListener('click', (event) => {
+    var isClickInside = specifiedElement.contains(event.target);
+    if ( !event.target.closest(".modal-content") && formOpen == true) {
+        document.querySelector('.bg-modal').style.display = 'none';
+        formOpen = false;
+      }
+});
+
+
 let myLibrary = [];
 
 function Book() {
